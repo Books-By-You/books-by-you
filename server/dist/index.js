d7b42db2e03844af7ddcd47af899f19bb2a4b40f
ryan.harter <ryan.harter.c@gmail.com>
2021-06-26 12:05:31 -0600

    initial endpoints added

--- server/dist/index.js
+++ server/dist/index.js
@@ -8,4 +8,10 @@ require('dotenv').config({ path: '../.env' });
 const { SERVER_PORT } = process.env;
 const app = express_1.default();
 app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`));
+//Auth Endpoints - maybe temporary pending passport w/Oauth implementation
+// app.post('/api/auth/register', authCtrl.register);
+// app.post('/api/auth/login', authCtrl.login);
+// app.delete('/api/auth/logout', authCtrl.logout)
+// app.post('/api/auth/delete', authCtrl.delete)
+//Book Endpoints 
 //# sourceMappingURL=index.js.map
