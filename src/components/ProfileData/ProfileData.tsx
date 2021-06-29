da956afb85cf04e7dd3c2aaaba0928b44796304b
Alex Romo III <alexromo3rd@gmail.com>
2021-06-29 12:54:32 -0500

    Working on ProfileData layout

--- src/components/ProfileData/ProfileData.tsx
+++ src/components/ProfileData/ProfileData.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import './ProfileData.scss';
 
 const ProfileData: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className='profile-data-container'>
+      <div className='profile-data-header'>
+        <h3>Title</h3>
+        <h3>Reviews</h3>
+        <h3>Library</h3>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileData;
