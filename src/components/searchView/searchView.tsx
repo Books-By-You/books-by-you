44d0969df4ce088b929ed78c25461700f72edde8
Sithara Jayadev <sithara@Sitharas-MacBook-Pro.local>
2021-07-01 22:47:28 -0400

    search view

--- src/components/searchView/searchView.tsx
+++ src/components/searchView/searchView.tsx
@@ -4,8 +4,9 @@ import search from "./search.png";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import { SliderData } from "./SliderData";
-import "./searchView.scss"
-import BookCard from "../BookCard/BookCard"
+import { SliderData2 } from "./SliderData2";
+import "./searchView.scss";
+import BookCard from "../BookCard/BookCard";
 
 const SearchView: React.FC = () => {
   //need 15 "objects to fill array from back end"
@@ -13,10 +14,12 @@ const SearchView: React.FC = () => {
 
   function arrayMapper(slide: any, index: any) {
     return (
-            <BookCard 
-            title={slide.title}
-            image_url={slide.image}
-            rating={slide.rating}/>
+      <BookCard
+        title={slide.title}
+        image_url={slide.image}
+        rating={slide.rating}
+        description={slide.description}
+      />
     );
   }
 
@@ -25,26 +28,51 @@ const SearchView: React.FC = () => {
   }
 
   const listBooks = SliderData.map(arrayMapper);
+  const listBooks2 = SliderData2.map(arrayMapper);
   return (
+    
     <div className="searchView">
+      
       <div className="carousel-container">
-        <Carousel autoPlay={true}
-        infiniteLoop={true}
-        showThumbs={false}>
-          <div className="book-carousel-1">
-          {listBooks}
-          </div>
+     
+        <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false}>
+          <div className="book-carousel-1">{listBooks2}</div>
+
           <div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/71aFt4+OTOL.jpg" />
-            <p className="legend">Legend 2</p>
+            <img
+              className="pexels-cover"
+              src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2b20731b-c5cc-478f-89fb-1144dae59b4c/ddco802-08c091ea-7f5b-411c-bb9a-4a4b8c161d9f.png/v1/fill/w_1192,h_670,q_70,strp/riftflower_by_brillick_ddco802-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvMmIyMDczMWItYzVjYy00NzhmLTg5ZmItMTE0NGRhZTU5YjRjXC9kZGNvODAyLTA4YzA5MWVhLTdmNWItNDExYy1iYjlhLTRhNGI4YzE2MWQ5Zi5wbmciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.8ul2AIvneg5ntDbXqVfMyyTSERPtqd5tRugNaYbKzcw"
+            />
+            <p className="legend"></p>
           </div>
+
+          <div className="book-carousel-1">{listBooks2}</div>
+
           <div>
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/AngelsAndDemons.jpg/220px-AngelsAndDemons.jpg" />
-            <p className="legend">Legend 3</p>
+            <img
+              className="pexels-cover"
+              src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2b20731b-c5cc-478f-89fb-1144dae59b4c/dei00tb-25e0a99b-e6c1-416e-a7fb-65d3ac53563e.png/v1/fill/w_1024,h_656,q_80,strp/forest_flowers_by_brillick_dei00tb-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjU2IiwicGF0aCI6IlwvZlwvMmIyMDczMWItYzVjYy00NzhmLTg5ZmItMTE0NGRhZTU5YjRjXC9kZWkwMHRiLTI1ZTBhOTliLWU2YzEtNDE2ZS1hN2ZiLTY1ZDNhYzUzNTYzZS5wbmciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.ySOA-AINhTmYdIOMMHQKwNOyUnCXMWR2clkIGuFWSb4"
+            />
+
+            <p className="legend"></p>
+          </div>
+
+          <div className="book-carousel-1">{listBooks2}</div>
+
+          <div>
+            <img
+              className="pexels-cover"
+              src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2b20731b-c5cc-478f-89fb-1144dae59b4c/delf1gs-552c40f4-e67c-4704-8e7f-69d214a8b214.png/v1/fill/w_1192,h_670,q_70,strp/flowers_by_brillick_delf1gs-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvMmIyMDczMWItYzVjYy00NzhmLTg5ZmItMTE0NGRhZTU5YjRjXC9kZWxmMWdzLTU1MmM0MGY0LWU2N2MtNDcwNC04ZTdmLTY5ZDIxNGE4YjIxNC5wbmciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.TkOkSl219Z_ZzeE6BPgEQcFOB0bGPUAPkEL6s0w7vLg"
+            />
+
+            <p className="legend"></p>
           </div>
         </Carousel>
+        <div className="heading">  <h1> Our Popular Books!</h1></div>
       </div>
+      
       <div className="search_wrap">
+      
         <div className="search">
           <div className="search_field">
             <button className="searchbutton">
@@ -65,12 +93,16 @@ const SearchView: React.FC = () => {
                 <a href="#">Link 3</a>
               </div>
             </div>
-            {/* <div className={`gradient-bottom-border2`}></div> */}
+            <section className="gradient-line">
+              <div className={`gradient-bottom-border2`}></div>
+            </section>
           </div>
         </div>
-        <div className="search"></div>
-        <div className="search"></div>
-        <div className="search"></div>
+        <div className="search-result-container">
+          <div className="search-result"> {listBooks}</div>
+          {/* <div className="search"></div>
+          <div className="search"></div> */}
+        </div>
       </div>
     </div>
   );
