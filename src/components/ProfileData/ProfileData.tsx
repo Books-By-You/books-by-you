176ae801410e072435fc73381df1b27abeb171c8
Alex Romo III <alexromo3rd@gmail.com>
2021-07-03 10:44:30 -0500

    Using SliderData to generate bookList in profiledata component

--- src/components/ProfileData/ProfileData.tsx
+++ src/components/ProfileData/ProfileData.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import BookCard from '../BookCard/BookCard';
+import { SliderData } from '../searchView/SliderData';
 import './ProfileData.scss';
 
 const ProfileData: React.FC = () => {
+  const bookList = SliderData.map((book) => (
+    <BookCard
+      title={book.title}
+      description={book.description}
+      image_url={book.image}
+      rating={book.rating}
+    />
+  ));
+
   return (
     <div className='profile-data-container'>
       <div className='profile-data-header'>
@@ -10,12 +20,7 @@ const ProfileData: React.FC = () => {
         <h3>Reviews</h3>
         <h3>Library</h3>
       </div>
-      <BookCard />
-      <BookCard />
-      <BookCard />
-      <BookCard />
-      <BookCard />
-      <BookCard />
+      {bookList}
     </div>
   );
 };
