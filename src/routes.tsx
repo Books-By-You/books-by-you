f3906d7575ba364462e329bac2e3c041245b24e4
Alex Romo III <alexromo3rd@gmail.com>
2021-07-06 14:00:26 -0500

    completed merge with main

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/homeView';
 import Auth from './components/auth/authView';
 import Reading from './components/readingView';
-import Book from './components/bookView';
+import Book from './components/BookView/bookView';
 import Profile from './components/ProfileView/ProfileView';
 import Search from './components/searchView/searchView';
 import Publishing from './components/publishingView';
@@ -15,33 +14,10 @@ export default (
     <Route exact path='/' component={Home} />
     <Route exact path='/login' component={Auth} />
     <Route exact path='/reading' component={Reading} />
-    <Route exact path='/book' component={Book} />
+    <Route exact path='/book/:id' component={Book} />
     <Route exact path='/profile/:id' component={Profile} />
     <Route exact path='/search' component={Search} />
     <Route exact path='/publishing' component={Publishing} />
     <Route exact path='/new-chapter' component={ChapterNew} />
-=======
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Home from "./components/homeView";
-import Auth from "./components/auth/authView";
-import Reading from "./components/readingView";
-import Book from "./components/BookView/bookView";
-import Profile from "./components/ProfileView/ProfileView";
-import Search from "./components/searchView/searchView";
-import Publishing from "./components/publishingView";
-import ChapterNew from "./components/chapterPublishing";
-
-export default (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/login" component={Auth} />
-    <Route exact path="/reading" component={Reading} />
-    <Route exact path="/book/:id" component={Book} />
-    <Route exact path="/profile/:id" component={Profile} />
-    <Route exact path="/search" component={Search} />
-    <Route exact path="/publishing" component={Publishing} />
-    <Route exact path="/new-chapter" component={ChapterNew} />
->>>>>>> d95311608317c2bee812d3cbf7cf86d80be9ab5c
   </Switch>
 );
