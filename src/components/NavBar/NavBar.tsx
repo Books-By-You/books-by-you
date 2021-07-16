2294d9332627170dcffce366faabf58481071efc
Alex Romo III <alexromo3rd@gmail.com>
2021-07-16 13:41:30 -0500

    Navbar now pulls data from redux store. Navbar complete. Also added Quicksand font to index.css file

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,20 +1,34 @@
-import React, { useState, useEffect } from "react";
-import { FaUserCircle } from "react-icons/fa";
-import { BsBook } from "react-icons/bs";
-import "./NavBar.scss";
+import React, { useState, useEffect } from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+import { BsBook } from 'react-icons/bs';
+import Button from '../Button/Button';
+import { connect } from 'react-redux';
+import { logout } from '../../redux/reducers/userReducer';
+import { Link, useHistory } from 'react-router-dom';
+import './NavBar.scss';
 
-const NavBar: React.FC = () => {
+interface User {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  profileImage: string;
+}
+
+const NavBar: React.FC<{ user: User; logout: () => void }> = ({
+  user,
+  logout,
+}) => {
   const [toggleNavLinks, setToggleNavLinks] = useState(false);
+  const history = useHistory();
   const bottomBorderSize = toggleNavLinks
-    ? "four-links-wide"
-    : "three-links-wide";
-
-  // temporary variable until redux implemented
-  const loggedIn = false;
+    ? 'four-links-wide'
+    : 'three-links-wide';
 
   useEffect(() => {
     const showLogout = () => {
-      if (loggedIn) {
+      if (user.userId) {
         setToggleNavLinks(true);
       } else {
         setToggleNavLinks(false);
@@ -22,44 +36,51 @@ const NavBar: React.FC = () => {
     };
 
     showLogout();
-  }, [loggedIn]);
+  }, [user.userId]);
+
+  const handleLogout = () => {
+    logout();
+    history.push('/');
+  };
 
   return (
-    <nav className="navbar">
-      <div className="book-icon">
+    <nav className='navbar'>
+      <div className='book-icon'>
         <BsBook />
       </div>
-      <div className="nav-link-container">
-        <ul className="nav-links">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#/search">Read</a>
-          </li>
-          {!loggedIn && (
-            <li>
-              <a href="#/login">Login</a>
-            </li>
-          )}
-          {loggedIn && (
+      <div className='nav-link-container'>
+        <div className='nav-links'>
+          <Link to='/'>Home</Link>
+          <Link to='/search'>Read</Link>
+          {!user.userId && <Link to='/login'>Login</Link>}
+          {user.userId && (
             <>
-              <li>
-                <a href="#">Account</a>
-              </li>
-              <li>
-                <a href="#">Logout</a>
-              </li>
+              <Link to={`/profile/${user.userId}`}>Profile</Link>
+              <Button
+                label='Logout'
+                styleName='logout'
+                handleClick={handleLogout}
+              />
             </>
           )}
-        </ul>
+        </div>
         <div className={`gradient-bottom-border ${bottomBorderSize}`}></div>
       </div>
-      <div className="user-profile-icon">
+      <div className='user-profile-icon'>
         <FaUserCircle />
       </div>
     </nav>
   );
 };
 
-export default NavBar;
+const mapStateToProps = (reduxState: any) => {
+  return {
+    user: reduxState.userReducer,
+  };
+};
+
+const mapDispatchToProps = {
+  logout: logout,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
